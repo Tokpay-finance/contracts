916a1f7e496bff147ef38d917467c5d275781a63
nafkem <nafkem@gmail.com>
2024-04-12 23:07:55 +0100

    update functions

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,19 @@ const { ethers, run, network } = require("hardhat");
 
 async function main() {
   
-  const TBILLToken = "0xE02FE869AE80ea475439734Bd29e937aD774BbFC"
+  const TBILLToken = "0xe8765FDBf60659BF5e5C3C55A989a1771aAfAaE7"
   const token = "0x874069fa1eb16d44d622f2e0ca25eea172369bc1"
   
-  //TbillVault  contract deployed to 0x5e85Ee1c67d4486493A1931bEf5191157B5FA394
+  //TBILLToken  contract deployed to 0xe8765FDBf60659BF5e5C3C55A989a1771aAfAaE7
+  //TbillVault  contract deployed to 0x641B8a631f50c6540fdA1C713D64b0D56ee0b1Fc
 
+
+  // const TBILLToken  = await ethers.deployContract("TBILLToken",[]); 
+  // await TBILLToken.waitForDeployment();
+  
+  // console.log(
+  //   `TBILLToken  contract deployed to ${TBILLToken.target}`
+  // );
 
   const TbillVault  = await ethers.deployContract("TbillVault",[ TBILLToken, token]); 
   await TbillVault.waitForDeployment();
